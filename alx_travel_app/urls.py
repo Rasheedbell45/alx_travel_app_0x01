0b07ecbfce1a9fe50a7973141a661f3ed1fc83fe
Rasheed Toba Bello <rasheedbell45@gmail.com>
2025-07-30 06:24:15 +0100

    Create urls.py

---
+++ alx_travel_app/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Travel API",
+        default_version='v1',
+        description="API for managing travel listings and bookings",
+    ),
+    public=True,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+]
